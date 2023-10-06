28a1b628fe1438b6a85b2a8b04639eec30748632
Buğra Günaydın <109613283+gunaydinbugra@users.noreply.github.com>
2023-10-06 21:49:46 +0300

    Add files via upload

---
+++ Lab Assignment 3 - c#/SavingAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_3___c_
+{
+    public class SavingAccount : BankAccount
+    {
+    }
+}
