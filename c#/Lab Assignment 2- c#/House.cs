03df70e16db78547a54838fa76b191ba21afc8c4
Buğra Günaydın <109613283+gunaydinbugra@users.noreply.github.com>
2023-10-07 22:05:22 +0300

    Add files via upload

---
+++ c#/Lab Assignment 2- c#/House.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_2___c_
+{
+    public class House
+    {
+        public Room r1;
+        public Room r2;
+        public Resident person;
+        public int totalm2 { get; set; }
+        public House() 
+        {
+            r1 = new Room();
+            r2 = new Room();
+            person = new Resident();
+            totalm2 = 150;
+        }
+        public void addResident(Resident p)
+        {
+            if (person.name == null && person.lastname == null)
+                person = p;
+        }
+        public int calculateTotalm2()
+        {
+            totalm2 = r1.m2 + r2.m2;
+            return totalm2;
+        }
+    }
+}
---
+++ c#/Lab Assignment 2- c#/Resident.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_2___c_
+{
+    public class Resident
+    {
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public Resident() 
+        {
+            name = null;
+            lastname = null;
+        }
+        public void getResident()
+        {
+            Console.WriteLine("--------------------------Resident Information--------------------------");
+            Console.Write("Name: ");
+            string a = Console.ReadLine();
+            name = a;
+            Console.Write("Last name: ");
+            a = Console.ReadLine();
+            lastname = a;
+        }
+        public void printResident()
+        {
+            Console.WriteLine($"Resident Name: {name}, Resident Last name: {lastname}");
+            Thread.Sleep(750);
+        }
+    }
+}
