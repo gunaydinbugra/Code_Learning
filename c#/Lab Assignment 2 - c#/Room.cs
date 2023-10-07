bd3719d14fe9f81b189051560a2946f062e638ec
Buğra Günaydın <109613283+gunaydinbugra@users.noreply.github.com>
2023-10-07 22:06:56 +0300

    Add files via upload

---
+++ c#/Lab Assignment 2 - c#/Room.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_2___c_
+{
+    public class Room
+    {
+        public int m2 { set; get; }
+        public string balcony { set; get; }
+
+        public void getRoom()
+        {
+            
+            bool b = false;
+            while(b != true)
+            {
+ //               Console.Clear();
+                Console.WriteLine("--------------------------Room Information--------------------------");
+                Console.Write("Enter m2 of room: ");
+                string a = Console.ReadLine();
+                if (int.TryParse(a, out int x))
+                {
+                    m2 = x;
+                    b = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid value entered please try again");
+                    Thread.Sleep(750);
+                }
+            }
+            b = false;
+            while(b != true)
+            {
+                Console.Write("Does room has a balcony (yes or no): ");
+                string a = Console.ReadLine();
+                if(a == "yes" || a == "no")
+                {
+                    balcony = a;
+                    b = true;
+                }
+                else
+                {
+                    Console.WriteLine("please enter only yer or no");
+                    Thread.Sleep(750);
+                }
+            }
+        }
+        public void printRoom()
+        {
+            Console.WriteLine($"m2: {m2},balcony: {balcony}");
+            Thread.Sleep(750);
+        }
+    }
+}
