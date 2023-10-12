6dfefe4bac43b99e9cb10468da84f2642a2d20c6
Buğra Günaydın <109613283+gunaydinbugra@users.noreply.github.com>
2023-10-12 23:36:34 +0300

    Add files via upload

---
+++ c#/Lab Assignment 5 - c#/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_5___c_
+{
+    public class Calculator
+    {
+        public int[] arr1 { get; set; }
+        public int[] arr2 { get; set; }
+        public int length { get; set; }
+        public Calculator(int l)
+        {
+            length = l;
+            arr1 = new int[l];
+            arr2 = new int[l];
+        }
+
+        public int[] Calculate(char c)
+        {
+            int[] a = new int[length];
+            switch (c)
+            {
+                case '+':
+                    for (int i = 0; i < length; i++)
+                    {
+                        a[i] = arr1[i] + arr2[i];
+                    }
+                    break;
+                case '-':
+                    for (int i = 0; i < length; i++)
+                    {
+                        a[i] = arr1[i] - arr2[i];
+                    }
+                    break;
+                case '/':
+                    try
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            a[i] = arr1[i] / arr2[i];
+                        }
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Division by zero not allowed");
+                    }
+                    break;
+                case '*':
+                    for(int i = 0; i < length; i++)
+                    {
+                        a[i] = arr1[i] * arr2[i];
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Value Entered");
+                    break;
+            }
+            return a;
+        }
+    }
+}
---
+++ c#/Lab Assignment 5 - c#/Program.cs
@@ -0,0 +1,115 @@
+﻿using Lab_Assignment_5___c_;
+
+int[] arr = new int[4];
+Calculator c = new Calculator(1);
+bool a = false;
+while(a != true)
+{
+    Console.Write("Lenght of the number set: ");
+    string b = Console.ReadLine();
+    if(int.TryParse(b, out int x))
+    {
+        c = new Calculator(x);
+        a = true;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Value Entered");
+    }
+}
+int[] an = new int[c.length];
+Console.Write("Please enter first number set: ");
+for (int i = 0; i < c.length; i++)
+{
+bool t = false;
+    while (t != true)
+    {
+    string b = Console.ReadLine();
+    if (int.TryParse(b, out int x))
+        {
+        c.arr1[i] = x;
+        t = true;
+        }
+        else
+        {
+        Console.WriteLine("Invalid Value Entered");
+        }
+    }
+}
+Console.Write("Please enter second number set: ");
+for (int i = 0; i < c.length; i++)
+{
+    bool t = false;
+    while (t != true)
+    {
+        string b = Console.ReadLine();
+        if (int.TryParse(b, out int x))
+        {
+            c.arr2[i] = x;
+            t = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Value Entered");
+        }
+    }
+}
+a = false;
+while(a != true)
+{
+    Console.Write("\nChoose Operation (+) (-) (/) (*) (exit): ");
+    string l = Console.ReadLine();
+    switch (l)
+    {
+        case "+":
+            an = c.Calculate('+');
+            Console.Write("Answer: ");
+            for (int i = 0; i < c.length; i++)
+            {
+                Console.Write($"{an[i]} ");
+            }
+            break;
+        case "-":
+            an = c.Calculate('-');
+            Console.Write("Answer: ");
+            for (int i = 0; i < c.length; i++)
+            {
+                Console.Write($"{an[i]} ");
+            }
+            break;
+        case "*":
+            an = c.Calculate('*');
+            Console.Write("Answer: ");
+            for (int i = 0; i < c.length; i++)
+            {
+                Console.Write($"{an[i]} ");
+            }
+            break;
+        case "/":
+            an = c.Calculate('/');
+            Console.Write("Answer: ");
+            for (int i = 0; i < c.length; i++)
+            {
+                Console.Write($"{an[i]} ");
+            }
+            break;
+        case "exit":
+            a = true;
+            break;
+        default:
+            Console.WriteLine("Invalid Value Entered");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
